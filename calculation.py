3b716c81c56b20c0d40c24edb67829548776a379
SherekarSaurabh <sherekarsaurabh@gmail.com>
2023-12-21 23:54:06 +0530

    Added project calculation

---
+++ calculation.py
@@ -0,0 +1,10 @@
+
+from shapes import circle, rectangle
+
+# Calculate circle area and circumference
+radius = 5
+circle_area = circle.area(radius)
+circle_circumference = circle.circumference(radius)
+
+print(f"Circle area: {circle_area}")
+print(f"Circle circumference: {circle_circumference}")
